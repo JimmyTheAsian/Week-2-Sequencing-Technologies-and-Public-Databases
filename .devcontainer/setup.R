9cdda850eabcb9a68ba25474cb040bc5baeb85ea
jparedesmontero <jparedes@svsu.edu>
2025-01-23 02:00:30 -0500

    Update setup.R

--- .devcontainer/setup.R
+++ .devcontainer/setup.R
@@ -1,4 +1,8 @@
 if (!requireNamespace("BiocManager", quietly = TRUE)) {
   install.packages("BiocManager")
 }
+
 BiocManager::install(c("sangerseqR", "Biostrings"))
+
+# Install other packages as needed
+install.packages(c("tidyverse"))
